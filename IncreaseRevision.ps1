7ed52275b899e107819b711cf18cae5861d1329c
sashaleyzerzon <l.sasha124@gmail.com>
2023-01-19 23:19:54 +0200

    commit
    commit

---
+++ IncreaseRevision.ps1
@@ -0,0 +1,14 @@
+﻿param (
+    $csprojpath # path to the file i.e. 'C:\Users\...\Code\csproj powershell\MySmallLibrary.csproj'
+)
+
+
+
+$xml = [Xml] (Get-Content $csprojPath)
+$version = [Version] $xml.Project.PropertyGroup.Version
+$revision = $version.Revision+1 
+$Field = $PSVersionTable.PSVersion.GetType().GetField('_Revision','static,nonpublic,instance')
+$Field.SetValue($version,$revision)
+$xml.Project.PropertyGroup.Version = [string] $version
+$xml.Save($csprojpath)
+
