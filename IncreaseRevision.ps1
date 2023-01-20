62eeb4ba90b3f0a7d6051f62359986265c338318
sashaleyzerzon <l.sasha124@gmail.com>
2023-01-20 16:57:24 +0200

    Update IncreaseRevision.ps1

--- IncreaseRevision.ps1
+++ IncreaseRevision.ps1
@@ -3,12 +3,14 @@
 )
 
 
-
+Write-Host "CsprojPath: " $csprojpath
 $xml = [Xml] (Get-Content $csprojPath)
 $version = [Version] $xml.Project.PropertyGroup.Version
+Write-Host "Current Version: " $version
 $revision = $version.Revision+1 
 $Field = $PSVersionTable.PSVersion.GetType().GetField('_Revision','static,nonpublic,instance')
 $Field.SetValue($version,$revision)
+Write-Host "New Version: " $version
 $xml.Project.PropertyGroup.Version = [string] $version
 $xml.Save($csprojpath)
 
